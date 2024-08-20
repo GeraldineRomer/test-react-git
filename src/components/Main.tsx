7a3a2ead97f2ac27bfeaa309a4e6691080ad94b1
GeraldineRomer <geraldine.rome0104@gmail.com>
2024-08-20 17:05:08 -0500

    first commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Main.css';
+
+export default function Main() {
+  return (
+    <main className='main'>
+        <aside className='main__aside'>Categorías</aside>
+        <section className='main__products'></section>
+    </main>
+  )
+}
+
