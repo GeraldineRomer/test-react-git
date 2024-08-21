df8411d3f3ec092d2b03ad496ef4ce844f53ab29
jeanSaid <yansaid21@gmail.com>
2024-08-21 11:49:37 -0500

    raw cards

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import '../styles/Main.css';
+import ProductCard from './ProductCard';
 
 export default function Main() {
   return (
     <main className='main'>
         <aside className='main__aside'>Categorías</aside>
-        <section className='main__products'></section>
+        <section className='main__products'>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </section>
     </main>
   )
 }
